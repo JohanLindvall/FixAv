c965c869932da78d35b2d394cb089f4eb58922f5
Johan Lindvall <johan.lindvall@waizer.com>
2017-11-08 13:34:20 +0100

    1st

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace FixAv
+{
+    using System;
+    using System.ComponentModel;
+    using System.Diagnostics;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var process in Process.GetProcesses())
+            {
+                try
+                {
+                    if (process.MainModule.FileName.StartsWith(@"C:\Program Files (x86)\Sophos"))
+                    {
+                        if (process.PriorityClass != ProcessPriorityClass.Idle)
+                        {
+                            Console.WriteLine($@"Setting '{process.ProcessName}' ProcessPriorityClass.Idle");
+                            process.PriorityClass = ProcessPriorityClass.Idle;
+                        }
+                    }
+                }
+                catch (Win32Exception)
+                {
+                }
+                catch (InvalidOperationException)
+                {
+                }
+            }
+        }
+    }
+}
